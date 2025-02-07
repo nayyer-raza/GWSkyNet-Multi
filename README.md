d7f6da71fdbbefd8602437a8af208f5191eafdfb
Nayyer Raza <nayyer.raza@ligo.org>
2025-02-07 18:53:11 -0500

    Updated for GWSkyNet-Multi II

--- README.md
+++ README.md
@@ -1,33 +1,33 @@
 # GWSkyNet-Multi
 
-GWSkyNet-Multi is a multi-class CNN classifier developed for the classification of candidate gravitational-wave events detected by the LIGO and Virgo observatories. This repository contains the published models together with scripts and instructions on how to use the classifier.
+GWSkyNet-Multi is a deep learning model developed for the classification of candidate gravitational-wave events detected by the LIGO and Virgo observatories. This repository contains the published models together with scripts and instructions on how to use the classifier.
 
-The model uses limited information released in the low-latency public alerts to produce prediction scores indicating whether an event is a merger of two black holes (BBH), a merger involving a neutron star (NS), or a non-astrophysical glitch. This facilitates time-sensitive decisions about whether to perform electromagnetic follow-up of candidate events during LIGO-Virgo-KAGRA (LVK) observing runs.
+The model uses localization information released in the low-latency public alerts to produce classification probabilities indicating whether an event is a binary black hole (BBH) merger, a neutron star - black hole (NSBH) merger, a binary neutron star (BNS) merger, or a non-astrophysical transient noise event in the detectors known as a "glitch". The classification facilitates time-sensitive decisions about whether to perform electromagnetic follow-up of candidate events during LIGO-Virgo-KAGRA (LVK) observing runs.
 
-The details of the GWSkyNet-Multi model and its inner workings are described in [Abbott et al. (2022)](https://iopscience.iop.org/article/10.3847/1538-4357/ac5019) and [Raza et al. (2024)](https://iopscience.iop.org/article/10.3847/1538-4357/ad13ea).
+The latest model, GWSkyNet-Multi II, is described in [Raza et al. (2025)](https://www.arxiv.org/abs/2502.00297). Details of the original GWSkyNet-Multi model and its inner workings are described in [Abbott et al. (2022)](https://iopscience.iop.org/article/10.3847/1538-4357/ac5019) and [Raza et al. (2024)](https://iopscience.iop.org/article/10.3847/1538-4357/ad13ea).
 
 
 ## Inputs
 
-The input to the classifier is the sky map FITS file generated for the candidate LVK event by the rapid localization pipeline [BAYESTAR](https://journals.aps.org/prd/abstract/10.1103/PhysRevD.93.024013), and released to the public on [GraceDB](https://gracedb.ligo.org). Note that these BAYESTAR sky maps are only produced for candidate compact binary coalescence (CBC) sources that are found by one of the online modeled search pipelines (see [here](https://emfollow.docs.ligo.org/userguide/analysis/searches.html#modeled-search) for details), and not for unmodeled burst sources. Thus GWSkyNet-Multi can only be used for CBC candidate events.
+The input to the classifier is the sky localization FITS file generated for the candidate LVK event by the rapid localization pipeline [BAYESTAR](https://journals.aps.org/prd/abstract/10.1103/PhysRevD.93.024013), and released to the public on [GraceDB](https://gracedb.ligo.org). Note that these BAYESTAR localization maps are only produced for candidate compact binary coalescence (CBC) sources that are found by one of the online modeled search pipelines (see [here](https://emfollow.docs.ligo.org/userguide/analysis/searches.html#modeled-search) for details), and not for unmodeled burst sources. Thus GWSkyNet-Multi can only be used to predict for candidate CBC events. The model is also trained on multi-detector events only, and so should not be used to predict on single detector events.
 
 ## Outputs
 
-In the prediction results that are saved, the output for each classifier is the mean score of 20 trained models used, with the standard deviation of the 20 model scores quoted as the uncertainty (ensemble uncertainty estimate). The given final classification is based on applying score thresholds from training to the model results in a hierarchical order, and selecting the classification that occurs the most often.
+The final classifier outputs are the prediction probabilities of the four possible classes (Glitch, BBH, NSBH, BNS), averaged over 20 trained models, with the standard deviation of the 20 model predictions quoted as the uncertainty (ensemble uncertainty estimate). A classification label is provided by selecting the class with the highest predicted probability (users can apply other label selection criteria depending on their science use case).
 
 
 ## Contents of this repository and how to use it
 
-- The "data" folder contains the confidence thresholds that were determined for each model for which the False Negative Rate = False Positive Rate. The folder also contains the metadata from training that is necessary for normalizations.
-- The "models" folder contains the trained machine learning models for each of the Glitch-vs-all, NS-vs-all, and BBH-vs-all classifiers. There are trained 20 versions of each model.
+- The "data" folder contains the normalization factors (from the training data) that need to be applied to the model inputs.
+- The "models" folder contains the model file (json) for the model architecture and associated weights files (h5). There are 20 weights files corresponding to the final weights of the 20 trained models.
 - The "predictions" folder is where the results of the GWSkyNet-Multi predictions are saved as individual text files.
 - The "scripts" folder contains the python scripts that can be used to perform classification with GWSkyNet-Multi.
-- The "skymaps" folder is where the scripts download and save the BAYESTAR sky map FITS files for each candidate event.
+- The "skymaps" folder is where the scripts download and save the BAYESTAR localization FITS files for each candidate event.
 
 
 To set up the environment with the required packages you can use either the provided yml file (conda) or txt file (pip). The following commands should accomplish this: `$ conda env create --file gwskynet_multi_predictions.yml` or `$ python3 -m pip install -r requirements.txt`
 
-If the user has the candidate event name, then the script [GWSkyNet_Multi_predict](scripts/GWSkyNet_Multi_predict.py) can be executed with the event name provided as an argument, which will download the FITS file from GraceDB, make the predictions, and save the results. Run the script with `--help` to see other argument details.
+If the user has the candidate event name, then the script [GWSkyNet_Multi_predict](scripts/GWSkyNet_Multi_predict.py) can be executed with the event name provided as an argument, which will download the BAYESTAR FITS file from GraceDB, make the predictions, and save the results. Run the script with `--help` to see other argument details.
 
 If the user wishes to launch a listener that uses a GCN stream for LVK public alerts, and then make predictions once a candidate CBC event alert is issued, then the script [LVK_alert_stream_and_predict](scripts/LVK_alert_stream_and_predict.py) can be executed. Note that the user's GCN client ID and client secret for the stream must be provided. If these have not already been set up, see the "Account Creation and Credential Generation" section at https://emfollow.docs.ligo.org/userguide/tutorial/receiving/gcn.html.
 
@@ -35,24 +35,12 @@ If the user wishes to launch a listener that uses a GCN stream for LVK public al
 
 ### Architecture
 
-GWSkyNet-Multi architecture for each one-vs-all classifier is the same. The first two branches are convolutional neural networks with residual connections for image data. The shape of the input data is indicated in parenthesis, with N the number of examples in the training set. The numbers in the SeparableConv2D (SepConv) and MaxPool layers indicate the kernel size in pixels and the number of filters (when applicable). The number in the Dense layers indicates the number of units.
+The GWSkyNet-Multi II model, shown below, is a neural network classifier and has only three layers following the concatenated inputs: 1) a dense layer of 8 neurons, 2) a second dense layer of 8 neurons, and 3) the final output dense layer of 4 neurons, each of which outputs the probability of the event belonging to one of the four classes: Glitch, BBH, NSBH, and BNS.
 
-![](figures/architecture.jpg)
-
-### Hierarchical Classification
-
-As GWSkyNet-Multi is a series of three one-vs-all classifiers, we need to apply a hierarchical scheme to arrive at a single final classification. Both the individual classifier scores and the final classification is reported by the model.
-
-![](figures/hierarchical_flow.jpg)
-
-### Test Performance
-
-False Positive Rate (FPR) and False Negative Rate (FNR) as functions of the score threshold on the testing set. We consider a threshold value for each classifier where the FPR=FNR.
-
-![](figures/fnr_fpr_thresholds.jpg)
+![](figures/architecture.pdf)
 
 ### O3 Performance
 
-Predicted vs. true classification confusion matrix for the 77 CBC candidate events in the LVK third observing run (O3) for which a public alert was issued. The predicted classifications are the GWSkyNet-Multi hierarchical ones. The true classifications are taken to be the ones from the final LVK O3 event catalog [GWTC-3](https://journals.aps.org/prx/abstract/10.1103/PhysRevX.13.041039). GWSkyNet-Multi misclassifies 15 (20%) of these events.
+Predicted vs. true classification confusion matrix for the 75 multi-detector CBC candidate events in the LVK third observing run (O3) for which a public alert was issued. The true classifications are determined from the final LVK O3 event catalog [GWTC-3](https://journals.aps.org/prx/abstract/10.1103/PhysRevX.13.041039). GWSkyNet-Multi II correctly classifies 61/75 (81%) of O3 events.
 
-![](figures/O3_classifications.jpg)
+![](figures/O3_classifications_confusion_matrix.pdf)
--- scripts/GWSkyNet_Multi_predict.py
+++ scripts/GWSkyNet_Multi_predict.py
@@ -10,10 +10,10 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import healpy as hp
 from astropy.io import fits
-from ligo.skymap import io, distance
-from reproject import reproject_from_healpix
-from skimage import transform
+from ligo.skymap import io
+from scipy.stats import norm as gaussian
 from subprocess import run
+import json
 import os
 import sys
 import argparse
@@ -30,7 +30,7 @@ parser.add_argument('--output-path',
 parser.add_argument('--models-path', 
                     help='Provide the path to the directory which contains the models. If not provided, it is assumed that the directory exists in the project top-level working directory.')
 parser.add_argument('--data-path', 
-                    help='Provide the path to the directory which contains the relevant data from training used in processing the sky map data and making the predictions. If not provided, it is assumed that the directory exists in the project top-level working directory.')
+                    help='Provide the path to the directory which contains the relevant input normalizations data from training. If not provided, it is assumed that the directory exists in the project top-level working directory.')
 opts = parser.parse_args()
 
 
@@ -40,7 +40,7 @@ output_path = opts.output_path if opts.output_path else drive_path + '/predictio
 if not os.path.exists(output_path):
     os.mkdir(output_path)
 models_path = '{}/'.format(opts.models_path) if opts.models_path else drive_path + '/models/'
-training_data_path = '{}/'.format(opts.data_path) if opts.data_path else drive_path + '/data/'
+data_path = '{}/'.format(opts.data_path) if opts.data_path else drive_path + '/data/'
 
 if opts.skymap_file:
     skymap_file = opts.skymap_file
@@ -56,188 +56,97 @@ else:
 
 ## FUNCTIONALITY TO READ FITS FILE, PROCESS AND PREPARE DATA FOR GWSKYNET-MULTI INPUT
 
-target_header = fits.Header.fromstring("""
-NAXIS   =                    2
-NAXIS1  =                  360
-NAXIS2  =                  180
-CTYPE1  = 'RA---CAR'
-CRPIX1  =                180.5
-CRVAL1  =                180.0
-CDELT1  =                   -1
-CUNIT1  = 'deg     '
-CTYPE2  = 'DEC--CAR'
-CRPIX2  =                 90.5
-CRVAL2  =                  0.0
-CDELT2  =                    1
-CUNIT2  = 'deg     '
-COORDSYS= 'icrs    '
-""", sep='\n')
+# Helper functions to calculate the 90% credible region for the localization area and volume
+#--------------------------------------------------------------------------------------------
+def calculate_area_CR(fits_file, CR=0.9):
+    (prob, mu, sigma, norm), metadata = io.read_sky_map(fits_file, distances=True, nest=None)
+    pix_area = hp.nside2pixarea(hp.npix2nside(np.size(prob)), degrees=True)
+    cum_prob = np.cumsum(np.sort(prob)[::-1])
+    sky_area = (np.nonzero(cum_prob >= CR)[0][0]+1) * pix_area
+    return sky_area
 
-# Normalization factors (from the training) to normalize distances and Bayes factors
-data_table = pd.read_csv(training_data_path+'data_summary_table.csv')
-training_norms = {'mean_distance': np.max(data_table.DistMean), 'max_distance': np.max(data_table.DistMax),
-                  'skymap': np.max(data_table.ProbMax), 'vol0': np.max(data_table.Vol0Max),
-                  'vol1': np.max(data_table.Vol1Max), 'vol2': np.max(data_table.Vol2Max),
-                  'logBCI': np.max(data_table.LogBCI), 'logBSN': np.max(data_table.LogBSN)}
+def calculate_volume_CR(fits_file, CR=0.9):
+    (prob, mu, sigma, norm), metadata = io.read_sky_map(fits_file, distances=True, nest=None)
+    max_distance = metadata['distmean'] + 2.5 * metadata['diststd']
+
+    n_points = 100
+    npix = np.size(prob)
+    nside = hp.npix2nside(npix)
+    pixarea = hp.nside2pixarea(nside)
+    r_bins = np.linspace(0, max_distance**3,num=n_points+1)**(1/3)
+    r = np.linspace(0, max_distance**3,num=2*n_points+1)[1::2]**(1/3)
+    dV = r_bins[1]**3 * ((4*np.pi/3)/npix)
+    
+    npix_used = int(npix/5)
+    pix_used_inds = np.argsort(-prob)[:npix_used]
+    dV_probs = np.zeros((n_points,npix_used))
+    for j in range(n_points):
+        dV_probs[j] = prob[pix_used_inds] * norm[pix_used_inds] * gaussian(mu[pix_used_inds], sigma[pix_used_inds]).pdf(r[j]) * dV / pixarea
+    
+    dV_probs_sorted = -np.sort(-dV_probs,axis=None,kind='mergesort')
+    dV_probs_cumsum = np.cumsum(dV_probs_sorted)
+    volume = np.count_nonzero(dV_probs_cumsum <= CR) * dV
+    return volume
+#--------------------------------------------------------------------------------------------
+
+# Normalization factors (from the training) to normalize input values
+input_norms_file = data_path+'input_normalization_factors.json'
+with open(input_norms_file, 'r') as f:
+    input_norms = json.load(f)
 
 def prepare_data(fits_file):
     (prob, mu, sigma, norm), metadata = io.read_sky_map(fits_file, distances=True, nest=None)
     
     # Read and normalize metadata
-    meandist, std = metadata['distmean'], metadata['diststd']
-    maxdist = meandist + 2.5 * std
-    mean_distance = meandist / training_norms['mean_distance']
-    max_distance = maxdist / training_norms['max_distance']
-    logBCI = metadata['log_bci'] / training_norms['logBCI']
-    logBSN = metadata['log_bsn'] / training_norms['logBSN']
+    dist_mean = np.log10(metadata['distmean']) / input_norms['LogDistance']
+    dist_std = np.log10(metadata['diststd']) / input_norms['LogDistance']
+    logBCI = metadata['log_bci'] / input_norms['LogBCI']
+    if metadata['log_bsn'] < 100:
+        logBSN = metadata['log_bsn'] / input_norms['LogBSN']
+    else:
+        logBSN = 100 / input_norms['LogBSN']
+    
     network = metadata['instruments']
     dets = []
     for ifo in ['H1', 'L1', 'V1']:
         dets.append(1) if ifo in network else dets.append(0)
-    if np.sum(dets) < 2: print('WARNING: this is a single detector event. GWSkyNet-Multi is only trained to predict on multi-detector events. Results may be unreliable.')
-    
-    # Process sky map and volume images
-    img_data = dict()
-    img_cols = ['skymap', 'vol0', 'vol1', 'vol2']
+    if np.sum(dets) < 2:
+        print('WARNING: this is a single detector event. GWSkyNet-Multi is only trained to predict on multi-detector events. Results may be unreliable.')
 
-    # reproject skymap data to rectangle
-    with np.errstate(invalid='ignore'):
-        img, mask = reproject_from_healpix((prob, 'ICRS'), target_header,
-                                    nested=metadata['nest'], hdu_in=None,
-                                    order='bilinear', field=0)
-        img_data['skymap'] = img
-        
-    # calculate volume projections    
-    rot = np.ascontiguousarray(distance.principal_axes(prob, mu, sigma, nest=metadata['nest']))
-    prob_sums = np.zeros(3)
+    # Calculate the localization area and volume from the maps, and normalize
+    sky_area = np.log10(calculate_area_CR(fits_file, CR=0.9)) / input_norms['LogSkyArea90']
+    volume = np.log10(calculate_volume_CR(fits_file, CR=0.9)) / input_norms['LogVolume90']
     
-    imgwidth = 524 #dpi=300
-    s = np.linspace(-maxdist, maxdist, imgwidth)
-    Mpc2_per_pix = (s[1]-s[0])**2
-    xx, yy = np.meshgrid(s, s)
-    for iface, (axis0, axis1) in enumerate(((1,0), (0,2), (1,2))):
-        density = distance.volume_render(xx.ravel(), yy.ravel(), maxdist,
-                                            axis0, axis1, rot, metadata['nest'], prob, mu,
-                                            sigma, norm).reshape(xx.shape)
-        vol_prob = density*Mpc2_per_pix
-        prob_sums[iface] = np.sum(vol_prob)
-        vol_prob_ds = transform.downscale_local_mean(vol_prob, (4,4)) * 16.0
-        img_data['vol{}'.format(iface)] = vol_prob_ds
-        
-    # re-calculate volume projections with higher resolution
-    # if the probability sums are not within acceptable tolerance
-    if (np.min(prob_sums) <= 0.98 or np.max(prob_sums) >= 1.005):
-        imgwidth = 1048 #dpi=600
-        s = np.linspace(-maxdist, maxdist, imgwidth)
-        Mpc2_per_pix = (s[1]-s[0])**2
-        xx, yy = np.meshgrid(s, s)
-        for iface, (axis0, axis1) in enumerate(((1,0), (0,2), (1,2))):
-            density = distance.volume_render(xx.ravel(), yy.ravel(), maxdist,
-                                                axis0, axis1, rot, metadata['nest'], prob, mu,
-                                                sigma, norm).reshape(xx.shape)
-            vol_prob = density*Mpc2_per_pix
-            prob_sums[iface] = np.sum(vol_prob)
-            vol_prob_ds = transform.downscale_local_mean(vol_prob, (8,8)) * 64.0
-            img_data['vol{}'.format(iface)] = vol_prob_ds
-    
-    # Normalize sky map and volume images
-    norm_img, norms = dict(), dict()
-    
-    for column in img_cols:
-        # Normalize img data
-        norm = np.max(img_data[column])
-        img = img_data[column] / norm
-
-        # Downsize img data using maxpooling
-        x = np.reshape(img, (1, len(img), len(img[0]), 1))
-        # To avoid tensorflow warnings
-        x = tf.cast(x, tf.float32)
-        if column == 'skymap':
-            maxpool = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))
-        else:
-            maxpool = tf.keras.layers.MaxPooling2D(pool_size=(1, 1))
-        norm_img[column] = np.array(maxpool(x))
-        # Normalize norms
-        norms[column] = norm / training_norms[column]
-    
-    # Stack volume images
-    # img_data has shape (1, 131, 131, 1), we need to reshape to (1, 131, 131) for stacking
-    stacked_volume = np.stack([np.reshape(norm_img[column], (1, 131, 131)) for column in img_cols[1:]], axis=-1)
-    stacked_volnorms = np.stack([norms[column] for column in img_cols[1:]], axis=-1)
-    
-    # Stack distances
-    distances = np.stack((mean_distance, max_distance), axis=-1)
-    
-    return [stacked_volume, norm_img['skymap'], np.reshape(dets, (1,3)), np.reshape(distances, (1,2)),
-            np.reshape(norms['skymap'], (1,1)), np.reshape(stacked_volnorms, (1,3)), np.reshape(logBSN, (1,1)), np.reshape(logBCI, (1,1))]
+    # Combine all data for model input, in correct order and array shape
+    data = [np.reshape(volume, (1,1)), np.reshape(sky_area, (1,1)), np.reshape(dets, (1,3)), np.reshape(dist_mean, (1,1)), np.reshape(dist_std, (1,1)), np.reshape(logBSN, (1,1)), np.reshape(logBCI, (1,1))]
+    #data = [volume, sky_area, np.array(dets), dist_mean, dist_std, logBSN, logBCI]
+    return data
 
 
 ## FUNCTIONALITY TO MAKE THE GWSKYNET-MULTI PREDICTIONS
 
-model_types = ['Glitch', 'NS', 'BBH']
 n_models = 20
+output_classes = ['Glitch', 'BBH', 'NSBH', 'BNS']
 def make_predictions(data):
     
-    # Load all the models and their thresholds from training
-    model_names = []
-    model_thresholds = []
-    for model_type in model_types:
-        model_names.append([model_type+'_{}'.format(i) for i in range(n_models)])
-        model_thresholds.append(np.load(training_data_path+model_type+'_confidence_thresholds.npy')*100)
-    model_names = np.ravel(np.array(model_names))
-    model_thresholds = np.ravel(np.array(model_thresholds))
-
-    loaded_models = []
-    loaded_weights = []
-    for i, name in enumerate(model_names):
-        with open(models_path+name+'.json', 'r') as json_file:
-            loaded_models.append(tf.keras.models.model_from_json(json_file.read()))
-            loaded_weights.append(loaded_models[i].load_weights(models_path+name+'.h5'))
+    # Load all the models from training
+    models = []
+    #weights = []
+    model_names = np.array(['Multi-class_{}'.format(i+1) for i in range(n_models)])
+    for i,name in enumerate(model_names):
+        with open(models_path+'Multi-class.json', 'r') as json_file:
+            models.append(tf.keras.models.model_from_json(json_file.read()))
+            models[i].load_weights(models_path+name+'.h5')
     
     # Make the predictions for each model, and find the mean and standard deviation
-    prediction_scores = np.zeros(n_models*3)
-    for i,model in enumerate(loaded_models):
-        prediction_scores[i] = 100*tf.squeeze(model(data), [-1]).numpy()[0]
+    prediction_probs = np.zeros((n_models,4))
+    for i,model in enumerate(models):
+        prediction_probs[i] = 100 * model(data).numpy()
     
-    prediction_score_means = np.zeros(3)
-    prediction_score_errors = np.zeros(3)
-    for i, model_type in enumerate(model_types):
-        prediction_score_means[i] = np.mean(prediction_scores[i*n_models:i*n_models+n_models])
-        prediction_score_errors[i] = np.std(prediction_scores[i*n_models:i*n_models+n_models])
+    prediction_prob_means = np.mean(prediction_probs,axis=0)
+    prediction_prob_stds = np.std(prediction_probs,axis=0)
     
-    # Find the hierarchical prediction classifications
-    # (hier_order given as list of 3 integers signifying order, with 0=Glitch, 1=NS, 2=BBH)
-    hier_order = [0,1,2]
-    hierarchical_prediction_classes = []
-    for i in range(n_models):
-        cur_model_scores = prediction_scores[[i,i+n_models,i+n_models*2]]
-        cur_model_threholds = model_thresholds[[i,i+n_models,i+n_models*2]]
-        
-        if cur_model_scores[hier_order[0]] >= cur_model_threholds[hier_order[0]]:
-            hier_pred=model_types[hier_order[0]]
-
-        elif cur_model_scores[hier_order[1]] >= cur_model_threholds[hier_order[1]]:
-            hier_pred=model_types[hier_order[1]]
-
-        elif cur_model_scores[hier_order[2]] >= cur_model_threholds[hier_order[2]]:
-            hier_pred=model_types[hier_order[2]]
-
-        else: 
-            m = max((cur_model_scores[0]-cur_model_threholds[0]),  (cur_model_scores[1]-cur_model_threholds[1]), (cur_model_scores[2]-cur_model_threholds[2]))
-            if m == (cur_model_scores[0]-cur_model_threholds[0]):
-                hier_pred=model_types[0]
-            elif m == (cur_model_scores[1]-cur_model_threholds[1]):
-                hier_pred=model_types[1]
-            elif m == (cur_model_scores[2]-cur_model_threholds[2]):
-                hier_pred=model_types[2]
-        hierarchical_prediction_classes.append(hier_pred)
-    hierarchical_prediction_classes = np.array(hierarchical_prediction_classes)
-    hierarchical_class_counts = np.zeros(3)
-    for i, model_type in enumerate(model_types):
-        hierarchical_class_counts[i] = np.count_nonzero(hierarchical_prediction_classes == model_type)
-    final_hierarchical_class = model_types[np.argmax(hierarchical_class_counts)]
-    
-    return prediction_score_means, prediction_score_errors, hierarchical_class_counts
+    return prediction_prob_means, prediction_prob_stds
 
 
 ## PROCESS THE FILE TO PREPARE DATA, AND MAKE THE PREDICTIONS
@@ -245,15 +154,17 @@ print('Processing the data...')
 data = prepare_data(skymap_file)
 print('Making the predictions...')
 prediction_results = make_predictions(data)
-model_scores = prediction_results[0]
-model_scores_err = prediction_results[1]
-final_hierarchical_class = model_types[np.argmax(prediction_results[2])]
+prediction_probs = prediction_results[0]
+prediction_probs_err = prediction_results[1]
+# Provide the classification label determined by the class with the highest probability
+prediction_class = output_classes[np.argmax(prediction_probs)]
 
 ## SAVE THE PREDICTION RESULTS
 # Save the predictions for this event to a text file
 results_save_file = '{}/{}.txt'.format(output_path, event_name)
 with open(results_save_file, 'w') as f:
-    for i, model_type in enumerate(model_types):
-        f.write('{}-vs-all score: {:.0f} +/- {:.0f}\n'.format(model_type, model_scores[i], model_scores_err[i]))
-    f.write('Hierarchical classification: {}'.format(final_hierarchical_class))
+    f.write('Predicted probabilities (%):\n')
+    for i, class_type in enumerate(output_classes):
+        f.write('{}: {:.0f} +/- {:.0f}\n'.format(class_type, prediction_probs[i], prediction_probs_err[i]))
+    f.write('Classification: {}'.format(prediction_class))
 print('Done! The prediction results can be found in {}'.format(results_save_file))
